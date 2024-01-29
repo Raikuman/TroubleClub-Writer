cb2720730e4941ce9d9eb3e53b3f592ba4c9397f
Raikuman <40256283+Raikuman@users.noreply.github.com>
2024-01-29 15:42:43 -0800

    Create navbar on page with jQuery

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./index.html", "pages/conversations.html", "pages/interactions.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'tc-black':'#000714',
+                'tc-blue':'#00AEFF',
+                'tc-white':'#FBDBE9',
+                'tc-red':'#FD016F',
+                'tc-purple':'#2E0D44',
+            }
+        },
     },
     plugins: [],
 }
